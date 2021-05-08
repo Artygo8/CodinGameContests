81307deb2cd2a6675394fa17ab1e2271e73f4a59
Arthur Gossuin (goa) <goa@odoo.com>
2021-05-08 20:01:25 +0200

    NOW BASED ON CURRENT COST + different dynamic

--- CGSC2021.py
+++ CGSC2021.py
@@ -3,8 +3,11 @@ import math
 from enum import Enum
 import random
 
+# I was 945th
+
+DAYS_TO_SEED = 6
 MAX_AMONT_TREE = 7
-COMPLETE_TIME = 22
+COMPLETE_TIME = 23
 
 def debug(*s):
     print(*s, file=sys.stderr, flush=True)
@@ -16,7 +19,6 @@ class AT(Enum):
     SEED = "SEED"
     GROW = "GROW"
     COMPLETE = "COMPLETE"
-
 
 #  ____  _                       
 # |  _ \| | __ _ _   _  ___ _ __ 
@@ -31,11 +33,16 @@ class Player:
         self.sun = 0
         self.score = 0
         self.is_waiting = 0
+        self.trees = [] # these are positions
+
+    def __str__(self):
+        return f"{[tree for tree in self.trees]}"
 
     def update(self, sun, score, is_waiting=0):
         self.sun = sun
         self.score = score
         self.is_waiting = is_waiting
+        self.trees.clear()
 
     def get_input_actions(self):
         # Possible actions, probably useless
@@ -45,9 +52,43 @@ class Player:
             if action.type != AT.WAIT:
                 self.possible_actions.append(action)
 
-    def compute_possible_actions(self):
-        pass
+    def compute_possible_actions(self, bd):
+        computed_actions = []
+        for pos in self.trees:
+            if bd[pos].is_dormant:
+                continue
+            # seedables
+            seedable = set()
+            for _ in range(bd[pos].size):
+                for neigh in set(bd[pos].neighbors).union(seedable):
+                    seedable.add(neigh)
+            for s in seedable:
+                if bd[s].tree == False:
+                    computed_actions.append(Action(AT.SEED, s, pos))
+            # complete
+            if bd[pos].size == 3:
+                computed_actions.append(Action(AT.COMPLETE, pos))
+            # grow
+            else:
+                computed_actions.append(Action(AT.GROW, pos))
+        return computed_actions
 
+    def current_total_cost(self, bd):
+        total = 0
+        nb_by_size = [sum(pos for pos in self.trees if bd[pos].size == i) for i in range(4)]
+        for pos in self.trees:
+            if bd[pos].size == 3:
+                total += 4
+            if bd[pos].size == 2:
+                total += 7 + nb_by_size[3]
+                nb_by_size[3] += 1
+            if bd[pos].size == 1:
+                total += 3 + nb_by_size[2]
+                nb_by_size[2] += 1
+            if bd[pos].size == 0:
+                total += 1 + nb_by_size[1]
+                nb_by_size[1] += 1
+        return total
 
 #   ____     _ _ 
 #  / ___|___| | |
@@ -77,6 +118,7 @@ class Cell:
         self.tree = True
         self.size = size
         self.is_mine = is_mine
+        (foe, me)[is_mine].trees.append(self.cell_index)
         self.is_dormant = is_dormant
 
     def sun_points(self):
@@ -87,7 +129,6 @@ class Cell:
         self.tree = False
         self.shadow = 0
         self.tree_neigh = 0
-
 
 #     _        _   _             
 #    / \   ___| |_(_) ___  _ __  
@@ -194,6 +235,12 @@ class Board:
         return sum([(cell.tree and cell.is_mine) for cell in self])
 
 
+#   ____                      
+#  / ___| __ _ _ __ ___   ___ 
+# | |  _ / _` | '_ ` _ \ / _ \
+# | |_| | (_| | | | | | |  __/
+#  \____|\__,_|_| |_| |_|\___|
+#                             
 class Game:
     def __init__(self):
         self.day = 0
@@ -207,25 +254,20 @@ class Game:
         board.get_input()
         me.get_input_actions()
 
-    def compute_next_action(self):
-        debug([po for po in me.possible_actions])
+    def basic_compute_next_action(self):
 
         # order by case number
         me.possible_actions.sort(key=lambda x:x.target_cell_id, reverse=True)
-
         # Separate Actions
         complete_actions = [action for action in me.possible_actions if action.type == AT.COMPLETE]
         seed_actions = [action for action in me.possible_actions if action.type == AT.SEED]
         grow_actions = [action for action in me.possible_actions if action.type == AT.GROW]
-
         # remove COMPLETE
-        if self.day < COMPLETE_TIME and me.score < foe.score + 20 \
-            and not all([board[pos].size == 3 for pos in board.tree_pos if board[pos].is_mine]):
-            # and sum([(board[pos].size == 3, 0)[board[pos].is_mine] for pos in board.tree_pos]) < 5 \
+        if self.day < COMPLETE_TIME and me.score < foe.score + 20:
             complete_actions = []
 
         # remove SEED
-        if board.count_my_trees() >= MAX_AMONT_TREE - self.day // 3:
+        if self.day > DAYS_TO_SEED and me.sun < me.current_total_cost(board):
             seed_actions = []
 
         # sort
@@ -234,12 +276,9 @@ class Game:
 
         board.compute_tree_neigh()
         board.compute_shadows(self.day)
-        for cell in board:
-            if cell.shadow:debug(cell)
+
         self.possible_actions.sort(key=lambda x:board[x.target_cell_id].tree_neigh) # first the one with the less neigh
         self.possible_actions.sort(key=lambda x:board[x.target_cell_id].richness, reverse=True) # first the richest
-
-        debug([po for po in self.possible_actions])
 
         return complete_actions[0] if complete_actions else (self.possible_actions[0] if self.possible_actions else "WAIT")
 
@@ -252,6 +291,13 @@ me = Player()
 foe = Player()
 
 while True:
-    debug("nut =", game.nutrients, ", ")
+
     game.get_input()
-    print(game.compute_next_action())
+
+    debug("nut =", game.nutrients, ", ")
+    debug("me:", me)
+    debug("foe:", foe)
+
+
+
+    print(game.basic_compute_next_action())
