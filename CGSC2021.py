a13e48135e0c08223e6c58b8a899524f5ffafd36
Arthur Gossuin (goa) <goa@odoo.com>
2021-05-08 09:25:12 +0200

    920th

--- CGSC2021.py
+++ CGSC2021.py
@@ -19,19 +19,26 @@ class ActionType(Enum):
 
 
 # I should have all the infos on my cells.
-
 class Cell:
     def __init__(self, cell_index, richness, neighbors):
         self.cell_index = cell_index
         self.richness = richness
         self.neighbors = neighbors
+        self.tree = None # need to place the trees
+        self.shadow = 0 # need to place the shadows
+
+    def sun_points(self):
+        if self.tree and self.tree.is_mine and self.shadow < self.tree.size and self.richness:
+            return self.tree.size + (self.richness - 1) * 2
+
 
 class Tree:
-    def __init__(self, cell_index, size, is_mine, is_dormant):
-        self.cell_index = cell_index
+    def __init__(self, size, is_mine, is_dormant):
+        # self.cell_index = cell_index
         self.size = size
         self.is_mine = is_mine
-        self.is_dormant = is_dormant
+        self.is_dormant = is_dormant # after any action, becomes dormant
+
 
 class Action:
     def __init__(self, type, target_cell_id=None, origin_cell_id=None):
@@ -62,6 +69,7 @@ class Action:
         if split[0] == ActionType.COMPLETE.name:
             return Action(ActionType.COMPLETE, int(split[1]))
 
+
 class Game:
     def __init__(self):
         self.day = 0
@@ -76,8 +84,8 @@ class Game:
         self.foe_is_waiting = 0
 
         # first inputs
-        self.grid_size = int(input())
-        for i in range(self.grid_size):
+        self.board_size = int(input())
+        for _ in range(self.board_size):
             cell_index, richness, *neigh = map(int, input().split())
             self.board.append(Cell(cell_index, richness, neigh))
 
@@ -90,7 +98,8 @@ class Game:
         self.trees.clear()
         for _ in range(int(input())):
             cell_index, size, is_mine, is_dormant = map(int, input().split())
-            self.trees.append(Tree(cell_index, size, is_mine == 1, is_dormant))
+            self.board[cell_index] = Tree(size, is_mine, is_dormant)
+            self.trees.append(Tree(size, is_mine, is_dormant)) # I should not need that...
 
         self.possible_actions.clear()
         for _ in range(int(input())):
@@ -101,23 +110,23 @@ class Game:
     def compute_next_action(self):
         debug([po for po in self.possible_actions])
 
-        # remove SEED
-        if sum([tree.is_mine for tree in self.trees]) >= MAX_AMONT_TREE or self.day >= DAYS_TO_PLANT:
-            while self.possible_actions and self.possible_actions[0].type == ActionType.SEED:
-                self.possible_actions.pop(0)
+        # order by case number
+        self.possible_actions.sort(key=lambda x:x.target_cell_id, reverse=True)
+        debug([po for po in self.possible_actions])
 
         # remove COMPLETE
         if self.day < COMPLETE_TIME and (sum([(tree.size == 3, 0)[tree.is_mine] for tree in self.trees]) < 4):
             while self.possible_actions and self.possible_actions[0].type == ActionType.COMPLETE:
                 self.possible_actions.pop(0)
 
-        # order by case number
-        self.possible_actions.sort(key=lambda x:x.target_cell_id)
-        debug([po for po in self.possible_actions])
-
         # Put COMPLETE as First
         complete_actions = [action for action in self.possible_actions if action.type == ActionType.COMPLETE]
-        complete_actions.sort(key=lambda x:x.target_cell_id, reverse=True)
+        complete_actions.sort(key=lambda x:x.target_cell_id)
+
+        # remove SEED
+        if sum([tree.is_mine for tree in self.trees]) >= MAX_AMONT_TREE or self.day >= DAYS_TO_PLANT:
+            while self.possible_actions and self.possible_actions[0].type == ActionType.SEED:
+                self.possible_actions.pop(0)
 
         return complete_actions[0] if complete_actions else (self.possible_actions[0] if self.possible_actions else "WAIT")
 
