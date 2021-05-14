8f1d0401d59f5239c6f4b539fa3b898476b7a7f9
Arthur Gossuin (goa) <goa@odoo.com>
2021-05-14 18:30:14 +0200

    +/-170th

--- CGSC2021-backup.py
+++ CGSC2021-backup.py
@@ -1,11 +1,12 @@
 import sys
 import math
-import time
-import random
 from enum import Enum
+import time
+
+# I was 945th
 
 DAYS_TO_SEED = 6
-MAX_AMONT_TREE = 9
+MAX_AMONT_TREE = 8
 COMPLETE_TIME = 23
 
 
@@ -14,6 +15,7 @@ def debug(*s):
 
 
 class Chrono:
+
     def __init__(self):
         self.moment = time.perf_counter()
 
@@ -24,6 +26,7 @@ class Chrono:
         debug(legend, f': { (time.perf_counter() - self.moment) * 1000:0.4f}')
 
 
+# Action Type
 class AT(Enum):
     WAIT = "WAIT"
     SEED = "SEED"
@@ -32,6 +35,7 @@ class AT(Enum):
 
 
 class Simulation:
+
     def __init__(self):
         self.best_score = -1
 
@@ -72,10 +76,6 @@ class Simulation:
         return results
 
 
-# class MCTS:
-
-
-
 #  ____  _
 # |  _ \| | __ _ _   _  ___ _ __
 # | |_) | |/ _` | | | |/ _ \ '__|
@@ -106,9 +106,12 @@ class Player:
         self.is_waiting = is_waiting
 
     def get_input_actions(self):
+        # Possible actions, probably useless
         self.possible_actions.clear()
         for _ in range(int(input())):
-            self.possible_actions.append(Action.parse(input()))
+            action = Action.parse(input())
+            if action.type != AT.WAIT:
+                self.possible_actions.append(action)
 
 #   ____     _ _
 #  / ___|___| | |
@@ -130,6 +133,7 @@ class Cell:
         self.is_dormant = 0  # after any action, becomes dormant
 
         self.shadow = 0
+        self.tree_neigh = 0
 
     def __repr__(self):
         return self.__str__()
@@ -144,6 +148,7 @@ class Cell:
         new.is_mine = self.is_mine
         new.is_dormant = self.is_dormant
         new.shadow = self.shadow
+        new.tree_neigh = self.tree_neigh
         return new
 
     def put_tree(self, size=0, is_mine=1, is_dormant=0):
@@ -158,19 +163,20 @@ class Cell:
     def grow(self, player):
         self.size += 1
 
-    def complete(self, player):  # does not add the points.
+    def complete(self, player):
         self.reset()
+        player.score += game.nutrients  # not very clever
 
-    def sun_points(self):  # Shadows must be computed first
+    def sun_points(self):
         return (0, self.size + (self.richness - 1) * 2)[self.tree and self.size and self.is_mine and (self.shadow < self.size or self.shadow == 0)]
 
     def reset(self):
         self.tree = False
         self.size = 0
         self.is_mine = 0
-        self.is_dormant = 0
+        self.is_dormant = 0  # after any action, becomes dormant
         self.shadow = 0
-        # maybe I should remove the shadows too...
+        self.tree_neigh = 0
 
 #     _        _   _
 #    / \   ___| |_(_) ___  _ __
@@ -198,7 +204,7 @@ class Action:
         split = action_string.split(' ')
         return Action(AT[split[0]], *map(int, split[1:][::-1]))
 
-    def price(self, player, bd):
+    def price(self, player, bd):  # if sleep enters, crash.
         if self.type == AT.WAIT:
             return 0
         nb_by_size = [sum(cell.tree for cell in bd if (cell.size == i and cell.is_mine == player.itsme)) for i in range(5)]  # 5 to have 0
@@ -217,7 +223,9 @@ class Board:
     def __init__(self):
         self.board = []
         self.size = 0
+        self.tree_pos = []
 
+# ACCESS
     def __getitem__(self, key):
         return self.board[key]
 
@@ -241,19 +249,21 @@ class Board:
     def __str__(self):
         s = self
         return f'''
-             [{s[25].size if s[25].tree else ' '}|{s[24].size if s[24].tree else ' '}|{s[23].size if s[23].tree else ' '}|{s[22].size if s[22].tree else ' '}]
-            [{s[26].size if s[26].tree else ' '}|{s[11].size if s[11].tree else ' '}|{s[10].size if s[10].tree else ' '}|{s[9].size if s[9].tree else ' '}|{s[21].size if s[21].tree else ' '}]
-           [{s[27].size if s[27].tree else ' '}|{s[12].size if s[12].tree else ' '}|{s[3].size if s[3].tree else ' '}|{s[2].size if s[2].tree else ' '}|{s[8].size if s[8].tree else ' '}|{s[20].size if s[20].tree else ' '}]
-          [{s[28].size if s[28].tree else ' '}|{s[13].size if s[13].tree else ' '}|{s[4].size if s[4].tree else ' '}|{s[0].size if s[0].tree else ' '}|{s[1].size if s[1].tree else ' '}|{s[7].size if s[7].tree else ' '}|{s[19].size if s[19].tree else ' '}]
-           [{s[29].size if s[29].tree else ' '}|{s[14].size if s[14].tree else ' '}|{s[5].size if s[5].tree else ' '}|{s[6].size if s[6].tree else ' '}|{s[18].size if s[18].tree else ' '}|{s[36].size if s[36].tree else ' '}]
-            [{s[30].size if s[30].tree else ' '}|{s[15].size if s[15].tree else ' '}|{s[16].size if s[16].tree else ' '}|{s[17].size if s[17].tree else ' '}|{s[35].size if s[35].tree else ' '}]
-             [{s[31].size if s[31].tree else ' '}|{s[32].size if s[32].tree else ' '}|{s[33].size if s[33].tree else ' '}|{s[34].size if s[34].tree else ' '}]'''
+     [{s[25].size if s[25].tree else ' '}|{s[24].size if s[24].tree else ' '}|{s[23].size if s[23].tree else ' '}|{s[22].size if s[22].tree else ' '}]
+    [{s[26].size if s[26].tree else ' '}|{s[11].size if s[11].tree else ' '}|{s[10].size if s[10].tree else ' '}|{s[9].size if s[9].tree else ' '}|{s[21].size if s[21].tree else ' '}]
+   [{s[27].size if s[27].tree else ' '}|{s[12].size if s[12].tree else ' '}|{s[3].size if s[3].tree else ' '}|{s[2].size if s[2].tree else ' '}|{s[8].size if s[8].tree else ' '}|{s[20].size if s[20].tree else ' '}]
+  [{s[28].size if s[28].tree else ' '}|{s[13].size if s[13].tree else ' '}|{s[4].size if s[4].tree else ' '}|{s[0].size if s[0].tree else ' '}|{s[1].size if s[1].tree else ' '}|{s[7].size if s[7].tree else ' '}|{s[19].size if s[19].tree else ' '}]
+   [{s[29].size if s[29].tree else ' '}|{s[14].size if s[14].tree else ' '}|{s[5].size if s[5].tree else ' '}|{s[6].size if s[6].tree else ' '}|{s[18].size if s[18].tree else ' '}|{s[36].size if s[36].tree else ' '}]
+    [{s[30].size if s[30].tree else ' '}|{s[15].size if s[15].tree else ' '}|{s[16].size if s[16].tree else ' '}|{s[17].size if s[17].tree else ' '}|{s[35].size if s[35].tree else ' '}]
+     [{s[31].size if s[31].tree else ' '}|{s[32].size if s[32].tree else ' '}|{s[33].size if s[33].tree else ' '}|{s[34].size if s[34].tree else ' '}]
+'''
 
     def __copy__(self):
         bd = Board()
         for cell in self:
             bd.append(cell.__copy__())
         bd.size = self.size
+        bd.tree_pos = list(self.tree_pos)
         return bd
 
     def append(self, stuff):
@@ -269,15 +279,26 @@ class Board:
         # reset
         for cell in self:
             cell.reset()
+        self.tree_pos = []
+
         for _ in range(int(input())):
             cell_index, size, is_mine, is_dormant = map(int, input().split())
             self[cell_index].put_tree(size, is_mine, is_dormant)
+            self.tree_pos.append(cell_index)  # I should not need that...
 
 # COMPUTE
+    def compute_tree_neigh(self):
+        for pos in self.tree_pos:
+            for n in self[pos].neighbors:
+                if n >= 0:
+                    self[n].tree_neigh += 1
+
     def compute_shadows(self, day):
+        # reset the shadows
         for cell in self:
             cell.shadow = 0
-        for pos in range(self.size):
+        # compute shadows
+        for pos in self.tree_pos:
             height = self[pos].size
             for _ in range(height):
                 pos = self[pos].neighbors[day % 6]
@@ -290,6 +311,7 @@ class Board:
         computed_actions = [Action.parse('WAIT')]
         for cell in self:
             if not cell.is_dormant and cell.is_mine == player.itsme:
+                # seedables
                 seedable = set()
                 for _ in range(cell.size):
                     for neigh in set(cell.neighbors).union(seedable):
@@ -297,6 +319,7 @@ class Board:
                 for seed in seedable:
                     if not self[seed].tree and self[seed].richness > 0 and seed != -1:
                         computed_actions.append(Action(AT.SEED, seed, cell.cell_index))
+                # complete + grow
                 if cell.tree:
                     computed_actions.append(Action((AT.GROW, AT.COMPLETE)[cell.size == 3], cell.cell_index))
         return computed_actions
@@ -346,17 +369,6 @@ class Board:
     def count_trees(self, player):
         return sum([(cell.tree and cell.is_mine == player.itsme) for cell in self])
 
-    def count_trees_around(self, cell):
-        return sum([self[n].tree for n in cell.neighbors])
-
-    def tree_neigh(self, player):
-        neighs = set()
-        for cell in self:
-            if cell.tree and cell.is_mine == player.itsme:
-                for n in cell.neighbors:
-                    neighs.add(n)
-        return neighs
-
 
 #   ____
 #  / ___| __ _ _ __ ___   ___
@@ -367,83 +379,64 @@ class Board:
 
 class Game:
 
-    def __init__(self, day=0, nutrients=0, me=Player(itsme=True), foe=Player(itsme=False), board=Board()):
-        self.day = day
-        self.last_day = 23
-        self.nutrients = nutrients
-        self.me = me
-        self.foe = foe
-        self.board = board
+    global day
 
-    def __copy__(self):
-        return Game(self.day, self.nutrients, self.me.__copy__(), self.foe.__copy__(), self.board.__copy__())
+    def __init__(self):
+        self.nutrients = 0
 
     def get_input(self):
-        self.day = int(input())
         self.nutrients = int(input())
-        self.me.update(*map(int, input().split()))
-        self.foe.update(*map(int, input().split()))
-        self.board.get_input()
-        self.me.get_input_actions()  # This is useless but needed
+        me.update(*map(int, input().split()))
+        foe.update(*map(int, input().split()))
+        board.get_input()
+        me.get_input_actions()
 
-    def apply(self, action):
-        self.board.apply(action, self.me)
-        self.me.sun -= action.price(self.me, self.board)
-
-    def compute_next_action(self):
-        debug([po for po in self.me.possible_actions])
+    def basic_compute_next_action(self):
 
         # order by case number
-        self.me.possible_actions = [action for action in self.me.possible_actions if action.type != AT.WAIT]
-        self.me.possible_actions.sort(key=lambda x: x.target_cell_id, reverse=True)
-
+        me.possible_actions.sort(key=lambda x: x.target_cell_id, reverse=True)
         # Separate Actions
-        complete_actions = [action for action in self.me.possible_actions if action.type == AT.COMPLETE]
-        seed_actions = [action for action in self.me.possible_actions if action.type == AT.SEED]
-        grow_actions = [action for action in self.me.possible_actions if action.type == AT.GROW]
-
+        complete_actions = [action for action in me.possible_actions if action.type == AT.COMPLETE]
+        seed_actions = [action for action in me.possible_actions if action.type == AT.SEED]
+        grow_actions = [action for action in me.possible_actions if action.type == AT.GROW]
         # remove COMPLETE
-        if self.day < COMPLETE_TIME and self.me.score < self.foe.score + 20 \
-            and sum([(cell.size == 3, 0)[cell.is_mine] for cell in self.board]) < 5 \
-                and not all([cell.size == 3 for cell in self.board if cell.is_mine]):
+        if day < COMPLETE_TIME and me.score < foe.score + 20:
             complete_actions = []
 
         # remove SEED
-        if self.board.count_trees(self.me) >= MAX_AMONT_TREE - self.day // 3:
-            seed_actions = []
-        debug(self.board.tree_neigh(self.me))
-        seed_actions = [action for action in seed_actions if action.target_cell_id not in self.board.tree_neigh(self.me)]
+        # if len(me.trees) > MAX_AMONT_TREE and me.sun < me.current_total_cost(board):
+        #     seed_actions = []
+        seed_actions = [seed for seed in seed_actions if board[seed.target_cell_id].tree_neigh < 3]
 
         # sort
         complete_actions.sort(key=lambda x: x.target_cell_id, reverse=True)
         self.possible_actions = seed_actions + grow_actions
 
-        self.board.compute_shadows(self.day)
-        for cell in self.board:
-            if cell.shadow:
-                debug(cell)
-        self.possible_actions.sort(key=lambda x: self.board[x.target_cell_id].richness, reverse=True)  # first the richest
-
-        debug([po for po in self.possible_actions])
+        self.possible_actions.sort(key=lambda x: board[x.target_cell_id].tree_neigh)  # first the one with the less neigh
+        self.possible_actions.sort(key=lambda x: board[x.target_cell_id].richness, reverse=True)  # first the richest
 
         return complete_actions[0] if complete_actions else (self.possible_actions[0] if self.possible_actions else "WAIT")
 
-    def play(self):
 
-        self.board.get_first_input()  # important to call it once
-
-        while True:
-            self.get_input()
-            # print(game.basic_compute_next_action())
-            # for _ in simu.one_day_simu(self.board, self.me, self.day):
-            #     pass
-            print(self.compute_next_action())
-
-
-# The code
+game = Game()
 
 chrono = Chrono()
-simu = Simulation()
-main_game = Game()
 
-main_game.play()
+board = Board()
+board.get_first_input()  # only the first time
+
+simu = Simulation()
+
+me = Player(itsme=True)
+foe = Player(itsme=False)
+
+while True:
+
+    day = int(input())
+    game.get_input()
+
+    # print(game.basic_compute_next_action())
+    for si in simu.one_day_simu(board, me, day):
+        pass
+        # debug(si)
+    print(game.basic_compute_next_action())
